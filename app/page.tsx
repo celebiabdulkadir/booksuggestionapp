2b3de73444aeaab3bb8103ee5eb158ea8563b557
celebiabdulkadir <abdulkadircelebi.ac@gmail.com>
2024-06-06 06:03:36 +0300

    Refactor code formatting and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,41 @@
 "use client";
 
-import { useState, useRef, useEffect } from 'react';
-import Loading from './loading';
+import { useState, useRef, useEffect } from "react";
+import Loading from "./loading";
 
 const questions = [
-  { type: 'text', question: 'What is your preferred genre?', name: 'genre', maxLength: 50 },
-  { type: 'text', question: 'Who are your favorite authors? (You can list multiple)', name: 'authors', maxLength: 100 },
-  { type: 'text', question: 'What language do you prefer?', name: 'language', maxLength: 50 },
-  { type: 'text', question: 'What type of story are you in the mood for (e.g., adventure, romance, mystery)?', name: 'storyType', maxLength: 100 },
-  { type: 'text', question: 'Are there any specific themes or topics you enjoy (e.g., dystopian, space exploration)?', name: 'themes', maxLength: 100 },
+  {
+    type: "text",
+    question: "What is your preferred genre?",
+    name: "genre",
+    maxLength: 50,
+  },
+  {
+    type: "text",
+    question: "Who are your favorite authors? (You can list multiple)",
+    name: "authors",
+    maxLength: 100,
+  },
+  {
+    type: "text",
+    question: "What language do you prefer?",
+    name: "language",
+    maxLength: 50,
+  },
+  {
+    type: "text",
+    question:
+      "What type of story are you in the mood for (e.g., adventure, romance, mystery)?",
+    name: "storyType",
+    maxLength: 100,
+  },
+  {
+    type: "text",
+    question:
+      "Are there any specific themes or topics you enjoy (e.g., dystopian, space exploration)?",
+    name: "themes",
+    maxLength: 100,
+  },
 ];
 
 export default function Home() {
@@ -31,7 +58,10 @@ export default function Home() {
   const answerRef = useRef<HTMLDivElement | null>(null);
 
   const handleAnswer = (answer: string) => {
-    const newAnswers = { ...answers, [questions[currentQuestionIndex].name]: answer };
+    const newAnswers = {
+      ...answers,
+      [questions[currentQuestionIndex].name]: answer,
+    };
     setAnswers(newAnswers);
 
     if (currentQuestionIndex < questions.length - 1) {
@@ -45,15 +75,17 @@ export default function Home() {
   const fetchRecommendations = async (answers: Answer) => {
     setLoading(true); // Set loading to true before fetching
     try {
-      const response = await fetch('/api/recommend', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(answers)
+      const response = await fetch("/api/recommend", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(answers),
       });
       const data = await response.json();
-      setRecommendations(data[0].split('\n').map((rec: string) => rec.replace(/^\d+\.\s+/, '')));
+      setRecommendations(
+        data[0].split("\n").map((rec: string) => rec.replace(/^\d+\.\s+/, ""))
+      );
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
     setLoading(false); // Set loading to false after fetching
   };
@@ -66,46 +98,53 @@ export default function Home() {
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       const target = e.target as HTMLInputElement;
       handleAnswer(target.value.trim());
-      target.value = ''; // Clear the input field
+      target.value = ""; // Clear the input field
     }
   };
 
   useEffect(() => {
     if (answerRef.current) {
-      answerRef.current.scrollIntoView({ behavior: 'smooth' });
+      answerRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [answers]);
 
   return (
     <div className="flex flex-col text-black justify-center items-center min-h-screen bg-gray-100">
-      
-      <div className="w-full max-w-3xl flex flex-col justify-end bg-white p-8 rounded-lg shadow-lg ">
-      <h1 className="text-2xl font-bold mb-6 text-center shadow-md py-2">Book Suggestion App</h1>
-        <div className='shadow-md p-2 text-sm  rounded h-[30dvh] overflow-auto'>
-          {questions.slice(0, currentQuestionIndex + 1).map((q: Question, index) => (
-            <div key={index} className="mb-6">
-              <div className="font-bold mb-2">{q.question}</div>
-              <div>
-                {q.type === 'text' && !isCompleted && index === currentQuestionIndex && (
-                  <input
-                    type="text"
-                    maxLength={q.maxLength}
-                    placeholder="Type your answer..."
-                    className="w-full p-2 border border-gray-300 rounded-lg"
-                    onKeyDown={handleKeyDown}
-                  />
-                )}
-              </div>
-              {answers[q.name] && (
-                <div ref={answerRef} className="mt-2 italic">
-                  {answers[q.name]}
+      <div className="w-full max-w-3xl flex flex-col  bg-white p-8 h-full   rounded-lg shadow-lg ">
+        <h1 className="text-2xl font-bold mb-6 text-center shadow-md py-2">
+          Book Suggestion App
+        </h1>
+        <div className=" flex flex-col justify-end shadow-md p-2 text-sm  rounded h-[30dvh]">
+          <div className=" overflow-auto">
+            {questions
+              .slice(0, currentQuestionIndex + 1)
+              .map((q: Question, index) => (
+                <div key={index} className="mb-6">
+                  <div className="font-bold mb-2">{q.question}</div>
+                  <div>
+                    {q.type === "text" &&
+                      !isCompleted &&
+                      index === currentQuestionIndex && (
+                        <input
+                          type="text"
+                          maxLength={q.maxLength}
+                          placeholder="Type your answer..."
+                          className="w-full p-2 border border-gray-300 rounded-lg"
+                          onKeyDown={handleKeyDown}
+                        />
+                      )}
+                  </div>
+                  {answers[q.name] && (
+                    <div ref={answerRef} className="mt-2 italic">
+                      {answers[q.name]}
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-          ))}
+              ))}
+          </div>
         </div>
         {isCompleted && (
           <div className="mt-6 text-sm">
@@ -127,10 +166,14 @@ export default function Home() {
               <Loading />
             ) : (
               <>
-                <div className="font-bold my-4 text-lg shadow-md p-2 rounded">Here are your book recommendations:</div>
+                <div className="font-bold my-4 text-lg shadow-md p-2 rounded">
+                  Here are your book recommendations:
+                </div>
                 <ul className="list-disc shadow-md p-2 rounded">
                   {recommendations.map((rec: string, index) => (
-                    <li key={index} className="mb-2">{rec}</li>
+                    <li key={index} className="mb-2">
+                      {rec}
+                    </li>
                   ))}
                 </ul>
               </>
